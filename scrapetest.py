9d21f0c1863d1a441feffec7d38c820cda0d1427
Thea Foronda <tdcforonda@gmail.com>
2021-10-27 04:05:07 +0800

    made web scraper with regular expressions

--- scrapetest.py
+++ scrapetest.py
@@ -1,4 +1,10 @@
-from urlib.request import urlopen
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen("http://pythonscraping.com/pages/page1.html")
 print(html.read())
+
+soup = BeautifulSoup(html, 'html.parser')
+
+print(soup.findAll('<head>', 'magna'))
+print(soup.find('sint'))
