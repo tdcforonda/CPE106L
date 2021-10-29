8405e144dba8a6255ee4c6b30448a44c19a383f0
Thea Foronda <tdcforonda@gmail.com>
2021-10-29 19:02:03 +0800

    added pile.py and player.py

---
+++ src/player.py
@@ -0,0 +1,33 @@
+from PILE import Pile
+
+
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.playPile = Pile()
+        self.wonPile = Pile()
+
+    def playCard(self):
+        if self.playPile.getSize() == 0:
+            self.useWonPile()
+        if self.playPile.getSize() > 0:
+            return self.playPile.nextCard()
+        return None
+
+    def getName(self):
+        return self.name
+
+    def collectCard(self, card):
+        self.wonPile.addCard(card)
+
+    def collectCards(self, pile):
+        self.wonPile.addCards(pile)
+
+    def useWonPile(self):
+        self.playPile.clear()
+        self.playPile.addCards(self.wonPile)
+        self.wonPile.clear()
+
+    def numCards(self):
+        return self.playPile.getSize() + self.wonPile.getSize()
